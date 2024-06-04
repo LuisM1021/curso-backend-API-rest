dbaa4051ed5ba5eba7a44b16db9fa60331391c7a
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-06-04 11:10:38 -0600

    Refactoring. Single responsibility principle, class 9

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const routerApi = require ('./routes');
+
 const app = express();
 const port = 3000;
+
+routerApi(app);
 
 app.get('/',(req,res)=>{
   res.send('Hello from express');
@@ -8,38 +12,6 @@ app.get('/',(req,res)=>{
 
 app.get('/new-endpoint',(req,res)=>{
   res.send('Hello from the new endpoint')
-})
-
-app.get('/products',(req,res)=>{
-  res.json([
-    {
-      name: 'Product 1',
-      price: 200
-    },
-    {
-      name: 'Product 2',
-      price: 1200
-    }
-  ])
-})
-
-app.get('/products/:id',(req,res)=>{
-  const {id} = req.params;
-  res.json(
-    {
-      productId: id,
-      name: 'Product 1',
-      price: 200
-    }
-  )
-})
-
-app.get('/categories/:categoryId/products/:productId',(req,res)=>{
-  const {categoryId, productId} = req.params;
-  res.json({
-    categoryId,
-    productId
-  })
 })
 
 app.listen(port,()=>{
