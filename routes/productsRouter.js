746f7035545b0d4719d9cd29f3f692192e92a338
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-06-07 16:30:44 -0600

    Post, put, patch, delete, status code and services. Class 14

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,20 +1,13 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../services/productsService');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/',(req,res)=>{
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-      products.push({
-        name: faker.commerce.productName(),
-        price: parseInt(faker.commerce.price(),10),
-        image: faker.image.url()
-      })
-  }
-  res.json(products);
+  const products = service.find();
+  res.status(200).json(products);
 })
 
 router.get('/filter',(req,res) => {
@@ -23,13 +16,34 @@ router.get('/filter',(req,res) => {
 
 router.get('/:id',(req,res)=>{
   const {id} = req.params;
-  res.json(
-    {
-      productId: id,
-      name: 'Product 1',
-      price: 200
-    }
-  )
+  const product = service.findOne(id);
+  res.status(200).json(product);
+})
+
+router.post('/',(req,res)=>{
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  });
+})
+
+router.patch('/:id',(req,res)=>{
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id
+  })
+})
+
+router.delete('/:id',(req,res)=>{
+  const {id} = req.params;
+  res.json({
+    message: 'deleted',
+    id
+  })
 })
 
 module.exports = router;
