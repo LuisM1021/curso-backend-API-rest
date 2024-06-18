fba0b0041f2628bf5d56120f6f7a4a206acee9ad
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-06-18 10:43:39 -0600

    Class 11. CRUD in the userService

--- api/routes/usersRouter.js
+++ api/routes/usersRouter.js
@@ -17,16 +17,57 @@ router.get('/', async (req, res, next)=>{
   }
 })
 
+router.get('/:id',
+  validationHandler(getUserSchema, 'params'),
+  async(req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await service.findOne(id);
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+)
+
 router.post('/',
   validationHandler(createUserSchema, 'body'),
   async (req, res, next) => {
   try{
-    const { body } = req;
+    const body = req.body;
     const newUser = await service.create(body);
     res.status(201).json(newUser);
   }catch(error){
     next(error);
   }
 })
+
+router.patch('/:id',
+  validationHandler(getUserSchema, 'params'),
+  validationHandler(updateUserSchema, 'body'),
+  async(req, res, next)=>{
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const updatedUser = await service.update(id,body);
+      res.status(201).json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+)
+
+router.delete('/:id',
+  validationHandler(getUserSchema,'params'),
+  async(req, res, next)=>{
+    try {
+      const { id } = req.params;
+      const deletedUser = await service.delete(id);
+      res.json(deletedUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+)
 
 module.exports = router;
