ce8bbe940cec1fec9a9c5056c93cca936ed6c515
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-06-22 07:31:43 -0600

    Class 21. Migration to create table orders_products

---
+++ api/db/models/orderProductModel.js
@@ -0,0 +1,65 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { PRODUCTS_TABLE } = require('./productModel');
+const { ORDER_TABLE } = require('./orderModel');
+
+const ORDER_PRODUCT_TABLE = 'orders_products';
+
+const OrderProductSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  amount: {
+    allowNull: false,
+    type: DataTypes.INTEGER
+  },
+  orderId: {
+    allowNull: false,
+    field: 'order_id',
+    type: DataTypes.INTEGER,
+    references: {
+      model: ORDER_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+  },
+  productId: {
+    allowNull: false,
+    field: 'product_id',
+    type: DataTypes.INTEGER,
+    references: {
+      model: PRODUCTS_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+  },
+  createdAt: {
+    allowNull: false,
+    field: 'created_at',
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW
+  }
+}
+
+class OrderProduct extends Model{
+  static associate(models){
+    // this.belongsTo(models.Customer,{
+    //   as: 'customer'
+    // })
+  }
+
+  static config(sequelize){
+    return {
+      sequelize,
+      tableName: ORDER_PRODUCT_TABLE,
+      modelName: 'OrderProduct',
+      timestamps: false
+    }
+  }
+}
+
+module.exports = { ORDER_PRODUCT_TABLE, OrderProductSchema, OrderProduct };
